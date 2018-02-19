3857db82618e51ffc1e24191fe4a2e5c795275d0
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2018-02-19 15:22:58 +0900

    update sample sentences, add logic for test data

--- src/test-data/test-sentences.ts
+++ src/test-data/test-sentences.ts
@@ -1,202 +1,266 @@
 export const TEST_SENTENCES = [
     {
-        "sentenceID": 123,
-        "transcript": "You're my valentine!",
-        "errorCategory": "Homophone",
-        "words": [
-            {
-                "wordHeadID": 456,
-                "wordRootID": 567,
-                "wordInstanceOrthography": "YOU'RE",
-                "partOfSpeech": "contraction",
-                "sequence": 1
-            },
-            {
-                "wordHeadID": 100,
-                "wordRootID": 891,
-                "wordInstanceOrthography": "MY",
-                "partOfSpeech": "adjective",
-                "sequence": 2
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "VALENTINE",
-                "partOfSpeech": "noun",
-                "sequence": 3
-            }
-        ],
+        "sentenceID": 1,
+        "transcript": "I couldn't find it at first, but did eventually.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "transitive verbs",
+        "recommendation": "common transitive verbs",
         "distractors": [
             {
-                "sequence": 1,
-                "transcript": "Your",
-                "errorType": "substitution",
-                "word": {
-                    "wordHeadID": 789,
-                    "wordRootID": 442,
-                    "wordInstanceOrthography": "YOUR",
-                    "partOfSpeech": "adjective"
-                }
-            }
-        ]
-    },
-    {
-        "sentenceID": 456,
-        "transcript": "Where is Waldo?",
-        "errorCategory": "Deletion",
-        "words": [
-            {
-                "wordHeadID": 456,
-                "wordRootID": 567,
-                "wordInstanceOrthography": "WHERE",
-                "partOfSpeech": "contraction",
-                "sequence": 1
-            },
-            {
-                "wordHeadID": 100,
-                "wordRootID": 891,
-                "wordInstanceOrthography": "IS",
-                "partOfSpeech": "adjective",
-                "sequence": 2
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "WALDO",
-                "partOfSpeech": "noun",
-                "sequence": 3
-            }
-        ],
-        "distractors": [
-            {
-                "sequence": 2,
-                "errorType": "deletion"
-            }
-        ]
-    },
-    {
-        "sentenceID": 789,
-        "transcript": "This is a very long sentence.",
-        "errorCategory": "Deletion",
-        "words": [
-            {
-                "wordHeadID": 456,
-                "wordRootID": 567,
-                "wordInstanceOrthography": "THIS",
-                "partOfSpeech": "contraction",
-                "sequence": 1
-            },
-            {
-                "wordHeadID": 100,
-                "wordRootID": 891,
-                "wordInstanceOrthography": "IS",
-                "partOfSpeech": "adjective",
-                "sequence": 2
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "A",
-                "partOfSpeech": "noun",
-                "sequence": 3
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "VERY",
-                "partOfSpeech": "noun",
+                "errorType": "deletion",
                 "sequence": 4
-            },
-
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "LONG",
-                "partOfSpeech": "noun",
-                "sequence": 5
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "SENTENCE",
-                "partOfSpeech": "noun",
-                "sequence": 6
-            }
-        ],
-        "distractors": [
-            {
-                "sequence": 1,
-                "transcript": "These",
-                "errorType": "substitution",
-                "word": {
-                    "wordHeadID": 789,
-                    "wordRootID": 442,
-                    "wordInstanceOrthography": "THESE",
-                    "partOfSpeech": "pronoun"
-                }
             }
         ]
     },
     {
-        "sentenceID": 789,
-        "transcript": "Substitution in the middle of a sentence",
-        "errorCategory": "Spelling",
-        "words": [
+        "sentenceID": 2,
+        "transcript": "They are all in favor of our plan to change the rules.",
+        "grammarTopic": "prepositions",
+        "grammarSubTopic": "set phrases",
+        "recommendation": "\"in favor of\" as a set phrase",
+        "distractors": [
             {
-                "wordHeadID": 456,
-                "wordRootID": 567,
-                "wordInstanceOrthography": "SUBSTITUTION",
-                "partOfSpeech": "contraction",
-                "sequence": 1
-            },
-            {
-                "wordHeadID": 100,
-                "wordRootID": 891,
-                "wordInstanceOrthography": "IN",
-                "partOfSpeech": "adjective",
-                "sequence": 2
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "THE",
-                "partOfSpeech": "noun",
-                "sequence": 3
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "MIDDLE",
-                "partOfSpeech": "noun",
+                "errorType": "deletion",
                 "sequence": 4
-            },
+            }
+        ]
+    },
+    {
+        "sentenceID": 3,
+        "transcript": "The man that I sold my car to paid me in cash.",
+        "grammarTopic": "prepositions",
+        "grammarSubTopic": "verb + preposition",
+        "recommendation": "verb + preposition",
+        "distractors": [
             {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "OF",
-                "partOfSpeech": "noun",
-                "sequence": 5
-            },
+                "errorType": "deletion",
+                "sequence": 8
+            }
+        ]
+    },
+    {
+        "sentenceID": 4,
+        "transcript": "The thief who stole the car was arrested and taken to jail.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "grammatical voice",
+        "recommendation": "passive vs active voice",
+        "distractors": [
             {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "A",
-                "partOfSpeech": "noun",
-                "sequence": 6
-            },
-            {
-                "wordHeadID": 143,
-                "wordRootID": 999,
-                "wordInstanceOrthography": "SENTENCE",
-                "partOfSpeech": "noun",
+                "errorType": "deletion",
                 "sequence": 7
             }
-        ],
+        ]
+    },
+    {
+        "sentenceID": 5,
+        "transcript": "The boss said that he was going to give me a raise, but it was a lie.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "transitive verbs",
+        "recommendation": "indirect objects",
         "distractors": [
             {
-                "sequence": 4,
-                "transcript": "middel",
-                "errorType": "substitution"
+                "errorType": "deletion",
+                "sequence": 10
+            }
+        ]
+    },
+    {
+        "sentenceID": 6,
+        "transcript": "It was very fun at the party, I enjoyed it a lot.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "transitive verbs",
+        "recommendation": "common transitive verbs",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 10
+            }
+        ]
+    },
+    {
+        "sentenceID": 7,
+        "transcript": "If your bag is heavy you can put it on the chair over there.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "transitive verbs",
+        "recommendation": "common transitive verbs",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 9
+            }
+        ]
+    },
+    {
+        "sentenceID": 8,
+        "transcript": "Please tell me what to study for the test.",
+        "grammarTopic": "articles",
+        "grammarSubTopic": "definite articles",
+        "recommendation": "definite vs indefinite vs zero article",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 8
+            }
+        ]
+    },
+    {
+        "sentenceID": 9,
+        "transcript": "I fell down and hurt myself while hiking in the mountains.",
+        "grammarTopic": "pronouns",
+        "grammarSubTopic": "reflexive pronouns",
+        "recommendation": "me vs myself",
+        "distractors": [
+            {
+                "errorType": "substitution",
+                "transcript": "me",
+                "sequence": 6
+            }
+        ]
+    },
+    {
+        "sentenceID": 10,
+        "transcript": "He showed us all how the problem can be solved.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "grammatical voice",
+        "recommendation": "passive vs active voice",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 9
+            }
+        ]
+    },
+    {
+        "sentenceID": 11,
+        "transcript": "Tom isn't going to eat cake because he's on a diet.",
+        "grammarTopic": "articles",
+        "grammarSubTopic": "indefinite articles",
+        "recommendation": "definite vs indefinite vs zero article",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 10
+            }
+        ]
+    },
+    {
+        "sentenceID": 12,
+        "transcript": "The desk is so big that I can't carry it by myself.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "transitive verbs",
+        "recommendation": "common transitive verbs",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 10
+            }
+        ]
+    },
+    {
+        "sentenceID": 13,
+        "transcript": "All of the tickets for today's performance are sold out.",
+        "grammarTopic": "articles",
+        "grammarSubTopic": "definite articles",
+        "recommendation": "definite vs indefinite vs zero article",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 3
+            }
+        ]
+    },
+    {
+        "sentenceID": 14,
+        "transcript": "These two pictures are almost the same but this one is a little better.",
+        "grammarTopic": "articles",
+        "grammarSubTopic": "definite articles",
+        "recommendation": "definite vs indefinite vs zero article",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 6
+            }
+        ]
+    },
+    {
+        "sentenceID": 15,
+        "transcript": "By the time I got there I found that the computer had disappeared.",
+        "grammarTopic": "tense",
+        "grammarSubTopic": "past perfect",
+        "recommendation": "past perfect tense",
+        "distractors": [
+            {
+                "errorType": "substitution",
+                "transcript": "was",
+                "sequence": 12
+            }
+        ]
+    },
+    {
+        "sentenceID": 16,
+        "transcript": "She has been painting this picture for two years.",
+        "grammarTopic": "tense",
+        "grammarSubTopic": "past continuous",
+        "recommendation": "past continuous tense",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 3
+            }
+        ]
+    },
+    {
+        "sentenceID": 17,
+        "transcript": "He didn't seem to understand what we said.",
+        "grammarTopic": "pronouns",
+        "grammarSubTopic": "relative pronouns",
+        "recommendation": "that vs what vs which",
+        "distractors": [
+            {
+                "errorType": "substitution",
+                "transcript": "that",
+                "sequence": 6
+            }
+        ]
+    },
+    {
+        "sentenceID": 18,
+        "transcript": "Can you please tell me the meaning of \u2018coincidence\u2019?",
+        "grammarTopic": "articles",
+        "grammarSubTopic": "definite articles",
+        "recommendation": "definite vs indefinite vs zero article",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 6
+            }
+        ]
+    },
+    {
+        "sentenceID": 19,
+        "transcript": "The nurse spoke to all the people who were injured in the accident.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "subject verb agreement",
+        "recommendation": "subject verb agreement \/ passive vs active voice",
+        "distractors": [
+            {
+                "errorType": "substitution",
+                "transcript": "was",
+                "sequence": 9
+            }
+        ]
+    },
+    {
+        "sentenceID": 20,
+        "transcript": "Going on vacation with my friends was the most enjoyable thing this year.",
+        "grammarTopic": "verbs",
+        "grammarSubTopic": "gerunds",
+        "recommendation": "subject verb agreement \/ gerund phrases",
+        "distractors": [
+            {
+                "errorType": "substitution",
+                "transcript": "were",
+                "sequence": 7
             }
         ]
     }
