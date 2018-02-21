7bcb6a1a2cbe8d96ffeab76e2ff2d473deb895af
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2018-02-21 13:38:06 +0900

    - new test data - add number markers

--- src/test-data/test-sentences.ts
+++ src/test-data/test-sentences.ts
@@ -1,100 +1,13 @@
 export const TEST_SENTENCES = [
     {
         "sentenceID": 1,
-        "transcript": "I couldn't find it at first, but did eventually.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "common transitive verbs",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 4
-            }
-        ]
-    },
-    {
-        "sentenceID": 2,
-        "transcript": "They are all in favor of our plan to change the rules.",
-        "grammarTopic": "prepositions",
-        "grammarSubTopic": "set phrases",
-        "recommendation": "\"in favor of\" as a set phrase",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 4
-            }
-        ]
-    },
-    {
-        "sentenceID": 3,
-        "transcript": "The man that I sold my car to paid me in cash.",
-        "grammarTopic": "prepositions",
-        "grammarSubTopic": "verb + preposition",
-        "recommendation": "verb + preposition",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 8
-            }
-        ]
-    },
-    {
-        "sentenceID": 4,
-        "transcript": "The thief who stole the car was arrested and taken to jail.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "grammatical voice",
-        "recommendation": "passive vs active voice",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 7
-            }
-        ]
-    },
-    {
-        "sentenceID": 5,
-        "transcript": "The boss said that he was going to give me a raise, but it was a lie.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "indirect objects",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 10
-            }
-        ]
-    },
-    {
-        "sentenceID": 6,
-        "transcript": "It was very fun at the party, I enjoyed it a lot.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "common transitive verbs",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 10
-            }
-        ]
-    },
-    {
-        "sentenceID": 7,
-        "transcript": "If your bag is heavy you can put it on the chair over there.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "common transitive verbs",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 9
-            }
-        ]
-    },
-    {
-        "sentenceID": 8,
         "transcript": "Please tell me what to study for the test.",
-        "grammarTopic": "articles",
-        "grammarSubTopic": "definite articles",
+        "lexicalCategory": "articles",
+        "grammaticalCategory": "definite articles",
+        "CEFR": {
+            "id": "283",
+            "level": "A1"
+        },
         "recommendation": "definite vs indefinite vs zero article",
         "distractors": [
             {
@@ -104,10 +17,137 @@ export const TEST_SENTENCES = [
         ]
     },
     {
+        "sentenceID": 2,
+        "transcript": "I couldn't find it at first, but did eventually.",
+        "lexicalCategory": "pronouns",
+        "grammaticalCategory": "transitive verbs",
+        "CEFR": {
+            "id": "922",
+            "level": "A1"
+        },
+        "recommendation": "common transitive verbs",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 4
+            }
+        ]
+    },
+    {
+        "sentenceID": 3,
+        "transcript": "If your bag is heavy you can put it on the chair over there.",
+        "lexicalCategory": "pronouns",
+        "grammaticalCategory": "transitive verbs",
+        "CEFR": {
+            "id": "922",
+            "level": "A1"
+        },
+        "recommendation": "common transitive verbs",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 9
+            }
+        ]
+    },
+    {
+        "sentenceID": 4,
+        "transcript": "I want to know what she will be in the future.",
+        "lexicalCategory": "verbs",
+        "grammaticalCategory": "modal verbs",
+        "CEFR": {
+            "id": "382",
+            "level": "A1"
+        },
+        "recommendation": "\"will\" + future time",
+        "distractors": [
+            {
+                "errorType": "deletion",
+                "sequence": 7
+            }
+        ]
+    },
+    {
+        "sentenceID": 5,
+        "transcript": "Tom sometimes goes fishing at the lake nearby.",
+        "lexicalCategory": "prepositions",
+        "grammaticalCategory": "prepositions of movement",
+        "CEFR": {
+            "id": "680",
+            "level": "A2"
+        },
+        "recommendation": "~ing activities are not places",
+        "distractors": [
+            {
+                "errorType": "insertion",
+                "transcript": "to",
+                "sequence": 4
+            }
+        ]
+    },
+    {
+        "sentenceID": 6,
+        "transcript": "A very exciting thing happened to me the other day.",
+        "lexicalCategory": "verbs",
+        "grammaticalCategory": "grammatical tense",
+        "CEFR": {
+            "id": "800",
+            "level": "B2"
+        },
+        "recommendation": "past simple tense",
+        "distractors": [
+            {
+                "errorType": "insertion",
+                "transcript": "was",
+                "sequence": 5
+            }
+        ]
+    },
+    {
+        "sentenceID": 7,
+        "transcript": "I made them give me the money back that they had stolen from my bag.",
+        "lexicalCategory": "verbs",
+        "grammaticalCategory": "bare infinitives",
+        "CEFR": {
+            "id": "1088",
+            "level": "B1"
+        },
+        "recommendation": "Subject + make + object + bare infinitive (infinitive without 'to')",
+        "distractors": [
+            {
+                "errorType": "insertion",
+                "transcript": "to",
+                "sequence": 4
+            }
+        ]
+    },
+    {
+        "sentenceID": 8,
+        "transcript": "I grew up in a small town in the countryside in Kyushu.",
+        "lexicalCategory": "verbs",
+        "grammaticalCategory": "grammatical tense",
+        "CEFR": {
+            "id": "784",
+            "level": "A1"
+        },
+        "recommendation": "past simple tense",
+        "distractors": [
+            {
+                "errorType": "insertion",
+                "transcript": "was",
+                "sequence": 2
+            }
+        ]
+    },
+    {
         "sentenceID": 9,
         "transcript": "I fell down and hurt myself while hiking in the mountains.",
-        "grammarTopic": "pronouns",
-        "grammarSubTopic": "reflexive pronouns",
+        "lexicalCategory": "pronouns",
+        "grammaticalCategory": "reflexive pronouns",
+        "CEFR": {
+            "id": "908",
+            "level": "A2"
+        },
         "recommendation": "me vs myself",
         "distractors": [
             {
@@ -119,74 +159,31 @@ export const TEST_SENTENCES = [
     },
     {
         "sentenceID": 10,
-        "transcript": "He showed us all how the problem can be solved.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "grammatical voice",
-        "recommendation": "passive vs active voice",
+        "transcript": "It was really hard to climb all of the way up Mount Fuji.",
+        "lexicalCategory": "pronouns",
+        "grammaticalCategory": "dummy pronouns",
+        "CEFR": {
+            "id": "931",
+            "level": "B1"
+        },
+        "recommendation": "dummy pronouns",
         "distractors": [
             {
-                "errorType": "deletion",
-                "sequence": 9
+                "errorType": "substitution",
+                "transcript": "I",
+                "sequence": 1
             }
         ]
     },
     {
         "sentenceID": 11,
-        "transcript": "Tom isn't going to eat cake because he's on a diet.",
-        "grammarTopic": "articles",
-        "grammarSubTopic": "indefinite articles",
-        "recommendation": "definite vs indefinite vs zero article",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 10
-            }
-        ]
-    },
-    {
-        "sentenceID": 12,
-        "transcript": "The desk is so big that I can't carry it by myself.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "common transitive verbs",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 10
-            }
-        ]
-    },
-    {
-        "sentenceID": 13,
-        "transcript": "All of the tickets for today's performance are sold out.",
-        "grammarTopic": "articles",
-        "grammarSubTopic": "definite articles",
-        "recommendation": "definite vs indefinite vs zero article",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 3
-            }
-        ]
-    },
-    {
-        "sentenceID": 14,
-        "transcript": "These two pictures are almost the same but this one is a little better.",
-        "grammarTopic": "articles",
-        "grammarSubTopic": "definite articles",
-        "recommendation": "definite vs indefinite vs zero article",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 6
-            }
-        ]
-    },
-    {
-        "sentenceID": 15,
         "transcript": "By the time I got there I found that the computer had disappeared.",
-        "grammarTopic": "tense",
-        "grammarSubTopic": "past perfect",
+        "lexicalCategory": "verbs",
+        "grammaticalCategory": "grammatical tense",
+        "CEFR": {
+            "id": "718",
+            "level": "B2"
+        },
         "recommendation": "past perfect tense",
         "distractors": [
             {
@@ -197,84 +194,20 @@ export const TEST_SENTENCES = [
         ]
     },
     {
-        "sentenceID": 16,
-        "transcript": "She has been painting this picture for two years.",
-        "grammarTopic": "tense",
-        "grammarSubTopic": "past continuous",
-        "recommendation": "past continuous tense",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 3
-            }
-        ]
-    },
-    {
-        "sentenceID": 17,
-        "transcript": "He didn't seem to understand what we said.",
-        "grammarTopic": "pronouns",
-        "grammarSubTopic": "relative pronouns",
-        "recommendation": "that vs what vs which",
+        "sentenceID": 12,
+        "transcript": "How far is it from your house to the nearest station?",
+        "lexicalCategory": "adjectives",
+        "grammaticalCategory": "superlatives",
+        "CEFR": {
+            "id": "63",
+            "level": "A2"
+        },
+        "recommendation": "superlatives",
         "distractors": [
             {
                 "errorType": "substitution",
-                "transcript": "that",
-                "sequence": 6
-            }
-        ]
-    },
-    {
-        "sentenceID": 18,
-        "transcript": "Can you please tell me the meaning of \u2018coincidence\u2019?",
-        "grammarTopic": "articles",
-        "grammarSubTopic": "definite articles",
-        "recommendation": "definite vs indefinite vs zero article",
-        "distractors": [
-            {
-                "errorType": "deletion",
-                "sequence": 6
-            }
-        ]
-    },
-    {
-        "sentenceID": 19,
-        "transcript": "The nurse spoke to all the people who were injured in the accident.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "subject verb agreement",
-        "recommendation": "subject verb agreement \/ passive vs active voice",
-        "distractors": [
-            {
-                "errorType": "substitution",
-                "transcript": "was",
-                "sequence": 9
-            }
-        ]
-    },
-    {
-        "sentenceID": 20,
-        "transcript": "Going on vacation with my friends was the most enjoyable thing this year.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "gerunds",
-        "recommendation": "subject verb agreement \/ gerund phrases",
-        "distractors": [
-            {
-                "errorType": "substitution",
-                "transcript": "were",
-                "sequence": 7
-            }
-        ]
-    },
-    {
-        "sentenceID": 21,
-        "transcript": "I couldn't find it at first, but did eventually.",
-        "grammarTopic": "verbs",
-        "grammarSubTopic": "transitive verbs",
-        "recommendation": "common transitive verbs",
-        "distractors": [
-            {
-                "transcript": "of",
-                "errorType": "insertion",
-                "sequence": 4
+                "transcript": "near",
+                "sequence": 10
             }
         ]
     }
